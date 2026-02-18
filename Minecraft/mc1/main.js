3f257e55808811932c82c81518088e8bf6973d1e
Mahdi Najian tabriz <mahti.naji@gmail.com>
2026-02-18 23:58:03 +0330

    Update main.js

--- Minecraft/mc1/main.js
+++ Minecraft/mc1/main.js
@@ -1,5 +1,5 @@
 // const apiUrl = "http://94.182.170.153:8124/status?key=abc123";
-const apiUrl = "/mcapi/status2";
+const apiUrl = "/mcapi2/status";
 
 const statusDot = document.getElementById("statusDot");
 const serverStatus = document.getElementById("serverStatus");
@@ -252,6 +252,7 @@ if (serverIpEl) {
     }
   });
 }
+
 
 
 
